8b6ee3278835580f78449f9f44511fc83c9b2b42
fabiodonello <donellofabio@gmail.com>
2021-10-18 00:01:37 +0200

    Creato il passaggio nella pgina principale e creato menu generale

--- MainPage.py
+++ MainPage.py
@@ -1,5 +1,4 @@
 from tkinter import *
-from PIL import ImageTk
 
 class MainPage:
 
@@ -8,6 +7,47 @@ class MainPage:
         self.root.title("Instagram Tool")
         self.root.geometry("1200x600")
         self.root.resizable(False, False)
+
+        # Create main menu frame
+        frame_menu = Frame(self.root, bd=0, bg="black")
+        frame_menu.place(x=0, y=0, height=600, width=300)
+
+        # Set button of main menu
+
+        # Dashboard button
+        dashboard_button = Button(frame_menu, text="Dashboard", command="set")
+        dashboard_button.place(x=0, y=0, height=100, width=300)
+
+        # Follow and unfollow button
+        follow_unfollow_button = Button(frame_menu, text="Follow & Unfollow", command="set")
+        follow_unfollow_button.place(x=0, y=100, height=100, width=300)
+
+        # Like and dislike button
+        l_d_button = Button(frame_menu, text="Like & Dislike", command="set")
+        l_d_button.place(x=0, y=200, height=100, width=300)
+
+        # Direct button
+        direct_button = Button(frame_menu, text="Direct", command="set")
+        direct_button.place(x=0, y=300, height=100, width=300)
+
+        # Auto publish button
+        auto_publish_button = Button(frame_menu, text="Auto publish", command="set")
+        auto_publish_button.place(x=0, y=400, height=100, width=300)
+
+        # Statistics button
+        statistics_button = Button(frame_menu, text="Statistics", command="set")
+        statistics_button.place(x=0, y=500, height=100, width=300)
+
+
+
+
+
+
+
+
+
+
+
 
 
 
