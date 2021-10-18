56baed2154962786e209f34f9ac05527c051ff63
fabiodonello <donellofabio@gmail.com>
2021-10-18 20:26:48 +0200

    Inizio creazione gestione di ig

--- MainPage.py
+++ MainPage.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from instabot import Bot
 
 class MainPage:
 
@@ -37,6 +38,44 @@ class MainPage:
         # Statistics button
         statistics_button = Button(frame_menu, text="Statistics", command="set")
         statistics_button.place(x=0, y=500, height=100, width=300)
+
+        # Create Instagram login frame
+        instagram_account_frame = Frame(self.root, bd=5, bg="black")
+        instagram_account_frame.place(x=330, y=150, height=400, width=500)
+
+        # Set title of Instagram login frame
+        instagram_account_label = Label(instagram_account_frame, text="Login", bg="white")
+        instagram_account_label.place(x=20, y=25)
+
+        # Insertion box username & password of Instagram account
+        instagram_username = Entry(instagram_account_frame, bg="yellow")
+        instagram_username.place(x=20, y=75, height=50, width=300)
+        instagram_password = Entry(instagram_account_frame, bg="yellow")
+        instagram_password.place(x=20, y=140, height=50, width=300)
+
+        instagram_access_button = Button(instagram_account_frame, text="Login", command="set")
+        instagram_access_button.place(x=20, y=210, height=50, width=150)
+
+        def get_username_password(event):
+            instagram_account_username = instagram_username.get()
+            instagram_account_password = instagram_password.get()
+
+            return (instagram_account_username, instagram_account_password)
+
+
+        instagram_access_button.bind("<Button-1>", get_username_password)
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
