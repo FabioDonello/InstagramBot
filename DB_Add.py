a6ff47074378c1ae74180142d97360cf48671bec
fabiodonello <donellofabio@gmail.com>
2021-10-21 16:41:45 +0200

    Aggiornamenti grafica e db

--- DB_Add.py
+++ DB_Add.py
@@ -16,41 +16,36 @@ def create_connection(db_file):
 
     return conn
 
-def create_project(conn, project):
+
+def insert_data(conn, project):
     """
     Create a new project into the projects table
     :param conn:
     :param project:
     :return: project id
     """
-    sql = ''' INSERT INTO login(first_name, last_name, email)
-              VALUES(?,?,?) '''
+    sql = ''' INSERT INTO login(first_name, last_name, email, password)
+              VALUES(?,?,?,?) '''
     cur = conn.cursor()
     cur.execute(sql, project)
     conn.commit()
     return cur.lastrowid
 
 
-
-
-def main(first_name, last_name, email ):
+def main(first_name, last_name, email, password):
 
     database = r"/Users/fabiodonello/Desktop/Esame OOP/InstgramBot_0/DB/IgDB.db"
 
     # create a database connection
     conn = create_connection(database)
+
     with conn:
+        # Add elements to table
+        data = (first_name, last_name, email, password)
+        project_id = insert_data(conn, data)
 
-        # tasks
-        task_1 = (first_name, last_name, email);
 
-        # create a database connection
-        conn = create_connection(database)
-        with conn:
-            # create a new project
-            project = (first_name, last_name, email);
-            project_id = create_project(conn, project)
-            print("tutto ok")
+
 
 
 
