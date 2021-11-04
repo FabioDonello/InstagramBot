718364c1f8bbe40ec8b22f1ed47e3e5965ad26b7
fabiodonello <donellofabio@gmail.com>
2021-11-04 18:57:57 +0100

    Creazione grafiche di follow

--- MainPage.py
+++ MainPage.py
@@ -17,25 +17,25 @@ class MainPage:
         dashboard_button = Button(frame_menu, text="Dashboard", command="set")
         dashboard_button.place(x=0, y=0, height=100, width=300)
 
-        # Follow and unfollow button
-        follow_unfollow_button = Button(frame_menu, text="Follow & Unfollow", command="set")
-        follow_unfollow_button.place(x=0, y=100, height=100, width=300)
+        # Follow button
+        follow_button = Button(frame_menu, text="Follow", command="set")
+        follow_button.place(x=0, y=100, height=100, width=300)
+
+        # Unfollow button
+        unfollow_button = Button(frame_menu, text="Unfollow", command="set")
+        unfollow_button.place(x=0, y=200, height=100, width=300)
 
         # Like and dislike button
         l_d_button = Button(frame_menu, text="Like & Dislike", command="set")
-        l_d_button.place(x=0, y=200, height=100, width=300)
+        l_d_button.place(x=0, y=300, height=100, width=300)
 
         # Direct button
         direct_button = Button(frame_menu, text="Direct", command="set")
-        direct_button.place(x=0, y=300, height=100, width=300)
+        direct_button.place(x=0, y=400, height=100, width=300)
 
         # Auto publish button
         auto_publish_button = Button(frame_menu, text="Auto publish", command="set")
-        auto_publish_button.place(x=0, y=400, height=100, width=300)
-
-        # Statistics button
-        statistics_button = Button(frame_menu, text="Statistics", command="set")
-        statistics_button.place(x=0, y=500, height=100, width=300)
+        auto_publish_button.place(x=0, y=500, height=100, width=300)
 
         # Frame manege
         auto_publish_frame = Frame(self.root, bd=5, bg="white")
@@ -50,21 +50,19 @@ class MainPage:
         like_dislike_frame = Frame(self.root, bd=5, bg="white")
         like_dislike_frame.place(x=300, y=0, height=600, width=900)
 
-        follow_unfollow_frame = Frame(self.root, bd=5, bg="white")
-        follow_unfollow_frame.place(x=300, y=0, height=600, width=900)
+        follow_frame = Frame(self.root, bd=5, bg="white")
+        follow_frame.place(x=300, y=0, height=600, width=900)
 
         dashboard_frame = Frame(self.root, bd=5, bg="white")
         dashboard_frame.place(x=300, y=0, height=600, width=900)
 
-        # Page title
-        page_title_label = Label(self.root, text="Dashboard", bg="white")
-        page_title_label.place(x=350, y=50)
+
 
         def auto_publish(event):
 
             dashboard_frame.place(x=300, y=0, height=0, width=0)
 
-            follow_unfollow_frame.place(x=300, y=0, height=0, width=0)
+            follow_frame.place(x=300, y=0, height=0, width=0)
 
             like_dislike_frame.place(x=300, y=0, height=0, width=0)
 
@@ -73,31 +71,28 @@ class MainPage:
             statistics_frame.place(x=300, y=0, height=0, width=0)
 
             auto_publish_frame.place(x=300, y=0, height=600, width=900)
-            page_title_label["text"] = "Auto publish"
 
-        def statistics(event):
+        def unfollow(event):
             dashboard_frame.place(x=300, y=0, height=0, width=0)
 
-            follow_unfollow_frame.place(x=300, y=0, height=0, width=0)
+            follow_frame.place(x=300, y=0, height=0, width=0)
 
             like_dislike_frame.place(x=300, y=0, height=0, width=0)
 
             direct_frame.place(x=300, y=0, height=0, width=0)
 
             statistics_frame.place(x=300, y=0, height=600, width=900)
-            page_title_label["text"] = "Statistics"
 
             auto_publish_frame.place(x=300, y=0, height=0, width=0)
 
         def direct(event):
             dashboard_frame.place(x=300, y=0, height=0, width=0)
 
-            follow_unfollow_frame.place(x=300, y=0, height=0, width=0)
+            follow_frame.place(x=300, y=0, height=0, width=0)
 
             like_dislike_frame.place(x=300, y=0, height=0, width=0)
 
             direct_frame.place(x=300, y=0, height=600, width=900)
-            page_title_label["text"] = "Direct"
 
             statistics_frame.place(x=300, y=0, height=0, width=0)
 
@@ -107,10 +102,9 @@ class MainPage:
 
             dashboard_frame.place(x=300, y=0, height=0, width=0)
 
-            follow_unfollow_frame.place(x=300, y=0, height=0, width=0)
+            follow_frame.place(x=300, y=0, height=0, width=0)
 
             like_dislike_frame.place(x=300, y=0, height=600, width=900)
-            page_title_label["text"] = "Like and dislike"
 
             direct_frame.place(x=300, y=0, height=0, width=0)
 
@@ -118,12 +112,11 @@ class MainPage:
 
             auto_publish_frame.place(x=300, y=0, height=0, width=0)
 
-        def follow_unfollow(event):
+        def follow(event):
 
             dashboard_frame.place(x=300, y=0, height=0, width=0)
 
-            follow_unfollow_frame.place(x=300, y=0, height=600, width=900)
-            page_title_label["text"] = "Follow and unfollow"
+            follow_frame.place(x=300, y=0, height=600, width=900)
 
             like_dislike_frame.place(x=300, y=0, height=0, width=0)
 
@@ -133,13 +126,62 @@ class MainPage:
 
             auto_publish_frame.place(x=300, y=0, height=0, width=0)
 
+            # Set titles
+
+            follow_label = Label(follow_frame, text="Follow", bg="grey", fg="white")
+            follow_label.place(x=0, y=0, height=50, width=100)
+
+            # set options
+
+            # Start follower bot
+
+            start_follower_bot_button = Button(follow_frame, text="Start", command="set")
+            start_follower_bot_button.place(x=100, y=0, height=50, width=100)
+
+            # Save follower options button
+
+            save_follower_option_button = Button(follow_frame, text="Save", command="set")
+            save_follower_option_button.place(x=200, y=0, height=50, width=100)
+
+            hashtag_check_var = IntVar()
+            location_check_var = IntVar()
+            account_check_var = IntVar()
+
+            def hashtag_options():
+                if hashtag_check_var.get() == 1:
+                    follow_by_hashtag_check_button["bg"] = "green"
+                if hashtag_check_var.get() == 0:
+                    follow_by_hashtag_check_button["bg"] = "grey"
+
+            def location_option():
+                if location_check_var.get() == 1:
+                    follow_by_location_check_button["bg"] = "green"
+                if location_check_var.get() == 0:
+                    follow_by_location_check_button["bg"] = "grey"
+
+            def account_options():
+                if account_check_var.get() == 1:
+                    follow_by_account_check_button["bg"] = "green"
+                if account_check_var.get() == 0:
+                    follow_by_account_check_button["bg"] = "grey"
+
+            follow_by_hashtag_check_button = Checkbutton(follow_frame, text="Follow by hashtag:", bg="grey",
+                                                         variable=hashtag_check_var, command=hashtag_options)
+            follow_by_hashtag_check_button.place(x=10, y=75, width=900)
+
+            follow_by_location_check_button = Checkbutton(follow_frame, text="Follow by location:", bg="grey",
+                                                          variable=location_check_var, command=location_option)
+            follow_by_location_check_button.place(x=10, y=275, width=900)
+
+            follow_by_account_check_button = Checkbutton(follow_frame, text="Follow by account:", bg="grey",
+                                                         variable=account_check_var, command=account_options)
+            follow_by_account_check_button.place(x=10, y=475, width=900)
+
         def dashboard(event):
 
             dashboard_frame.place(x=300, y=0, height=600, width=900)
 
-            page_title_label["text"] = "Dashboard"
-
-            follow_unfollow_frame.place(x=300, y=0, height=0, width=0)
+            follow_frame.place(x=300, y=0, height=0, width=0)
 
             like_dislike_frame.place(x=300, y=0, height=0, width=0)
 
@@ -151,10 +193,10 @@ class MainPage:
 
 
         auto_publish_button.bind("<Button-1>", auto_publish)
-        statistics_button.bind("<Button-1>", statistics)
+        unfollow_button.bind("<Button-1>", unfollow)
         direct_button.bind("<Button-1>", direct)
         l_d_button.bind("<Button-1>", like_dislike)
-        follow_unfollow_button.bind("<Button-1>", follow_unfollow)
+        follow_button.bind("<Button-1>", follow)
         dashboard_button.bind("<Button-1>", dashboard)
 
 
